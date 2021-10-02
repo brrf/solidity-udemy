f10119a3b6294faaecfbb908e2c7b82c008cc077
Moshe Praver <moshepraver@gmail.com>
2021-10-02 14:33:26 -0500

    deploy inbox contract

---
+++ deploy.js
@@ -0,0 +1,24 @@
+const HDWalletProvider = require('truffle-hdwallet-provider');
+const Web3 = require('web3');
+const { interface, bytecode } = require('./compile');
+require('dotenv').config()
+
+const provider = new HDWalletProvider(
+    process.env.SEED,
+    'https://rinkeby.infura.io/v3/4e400ba27c504de585c4782b1bd50931'
+);
+const eth = new Web3(provider).eth;
+
+
+async function deploy () {
+    const accounts = await eth.getAccounts();
+    console.log(`Attempting deploy from account: ${accounts[0]}`)
+
+    const result = await new eth.Contract(JSON.parse(interface))
+        .deploy({ data: bytecode, arguments: ['jello whirl'] })
+        .send({ gas: '1000000', gasPrice: '5000000000', from: accounts[0] })
+
+    console.log(`Contract deployed to: ${result.options.address}`);
+}
+
+deploy();
